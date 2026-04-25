7964b31372832de076f347d1cfe39a5be1158c02
Diyorbek Safarov <124151930+CodeBloomuz@users.noreply.github.com>
2026-04-25 08:42:49 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "0").split(",")))
+BOT_TOKEN = os.getenv("8485266843:AAHHNIPcn9Hgf_iOQDEL0hT105goEwC6-CM")
+ADMIN_IDS = list(map(int, os.getenv("6551375195", "0").split(",")))
 DB_PATH = "ilc.db"
 TEMPLATE_PATH = "Ariza_Namuna.docx"
